26ec2541aa2f1e3730cc640ec6f2870bb5779e35
gotou1212 <meijiyou.20081212@gmail.com>
2025-03-11 01:50:43 +0000

    w

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,159 @@
+
+
+
 import React, { useState } from 'react';
 import './App.css';
-
 function App() {
   const [task, setTask] = useState('');
   const [tasks, setTasks] = useState([]);
-
   const handleInputChange = (e) => {
     setTask(e.target.value);
   };
-
   const handleAddTask = () => {
     if (task.trim()) {
       setTasks([...tasks, task]);
       setTask('');
     }
   };
-
   const handleDeleteTask = (index) => {
     const newTasks = tasks.filter((_, i) => i !== index);
     setTasks(newTasks);
   };
-
   return (
     <div>
       <div className="kakoi">
         <h3 className='TODOアプリ'>TODOアプリ</h3>
-        <div>
-          <div className="tekist">
-            <input
-              type="text"
-              value={task}
-              onChange={handleInputChange}
-              placeholder="タスクを入力..."
-            />
+        <div className="task-list">
+          <div>
+            <div className="tekist">
+              <input
+                type="text"
+                value={task}
+                onChange={handleInputChange}
+                placeholder="タスクを入力..."
+              />
+            </div>
+            <button onClick={handleAddTask} className="tuika-button">追加</button>
           </div>
-          <button onClick={handleAddTask} className="tuika-button">追加</button>
+          {tasks.map((task, index) => (
+            <div key={index} className="task-item">
+              <span>{task}</span>
+              <button onClick={() => handleDeleteTask(index)} className="delete-button">削除</button>
+            </div>
+          ))}
         </div>
-      </div>
-
-      <div className="task-list">
-        {tasks.map((task, index) => (
-          <div key={index} className="task-item">
-            <span>{task}</span>
-            <button onClick={() => handleDeleteTask(index)} className="delete-button">削除</button>
-          </div>
-        ))}
       </div>
     </div>
   );
 }
-
 export default App;
+import Image from "next/image";
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <Image
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+          <li className="mb-2">
+            Get started by editing{" "}
+            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+              src/app/page.tsx
+            </code>
+ .
+          </li>
+          <li>Save and see your changes instantly.</li>
+        </ol>
+
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <a
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className="dark:invert"
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={20}
+              height={20}
+            />
+            Deploy now
+          </a>
+          <a
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Read our docs
+          </a>
+        </div>
+      </main>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/file.svg"
+            alt="File icon"
+            width={16}
+            height={16}
+          />
+          Learn
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/window.svg"
+            alt="Window icon"
+            width={16}
+            height={16}
+          />
+          Examples
+        </a>
+        <a
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/globe.svg"
+            alt="Globe icon"
+            width={16}
+            height={16}
+          />
+          Go to nextjs.org →
+        </a>
+      </footer>
+    </div>
+  );
+}
+
+
+
+
+
+
+
+
+
+
